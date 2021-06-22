ae325c41cfddbd172aed9738aa797013fae7978a
4zv4l <simon.blacks.be@gmail.com>
2021-06-22 09:41:45 +0200

    change for main

--- main.c
+++ main.c
@@ -6,22 +6,20 @@
 
 int main(){
 	clear();
-	//perso **team = init();
-	perso **team = malloc(sizeof(perso)*3);
-
-	team[0] = malloc(sizeof(perso));
-	team[0]->nom = "Azz";
-	team[0]->armure = 1;
-	team[0]->degat = 1;
-	team[0]->mana = 1;
-	team[0]->maxPV = 1;
-
-	team[1] = malloc(sizeof(perso));
-	team[1]->nom = "Mega";
-	team[1]->armure = 1;
-	team[1]->degat = 1;
-	team[1]->mana = 1;
-	team[1]->maxPV = 1;
+	perso **team = init();
+			//perso **team = malloc(sizeof(perso)*3);
+			// team[0] = malloc(sizeof(perso));
+			// team[0]->nom = "Azz";
+			// team[0]->armure = 1;
+			// team[0]->degat = 1;
+			// team[0]->mana = 1;
+			// team[0]->maxPV = 1;
+			// team[1] = malloc(sizeof(perso));
+			// team[1]->nom = "Mega";
+			// team[1]->armure = 1;
+			// team[1]->degat = 1;
+			// team[1]->mana = 1;
+			// team[1]->maxPV = 1;
 	//show_team(team);
 	//show_intel(intel_mobs);
 	//show_semi(semi_mobs);
